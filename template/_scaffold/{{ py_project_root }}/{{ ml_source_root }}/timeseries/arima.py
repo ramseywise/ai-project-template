500d80b7d3c0d2a275f3afc22559d0b042bd7d02
ramseywise <ramsey.wise@gmail.com>
2026-07-28 14:54:59 +0200

    fix: update tests

--- template/_scaffold/{{ py_project_root }}/{{ ml_source_root }}/timeseries/arima.py
+++ template/_scaffold/{{ py_project_root }}/{{ ml_source_root }}/timeseries/arima.py
@@ -27,6 +27,7 @@ from dataclasses import dataclass, field
 from typing import Any
 
 import numpy as np
+
 from ml.timeseries.preprocessing import (
     StationarityReport,
     check_stationarity,
